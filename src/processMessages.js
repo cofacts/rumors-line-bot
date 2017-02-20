106bae144fdadd47491134fd001ecdd2372883a5
MrOrz <johnsonliang7@gmail.com>
2017-02-21 00:16:46 +0800

    Implement all cases.

--- src/processMessages.js
+++ src/processMessages.js
@@ -58,8 +58,8 @@ export default async function processMessages(
           },
         };
 
-        // Store articles
-        data.foundArticles = SearchArticles.edges.map(({ node }) => node);
+        // Store article ids
+        data.foundArticleIds = SearchArticles.edges.map(({ node: { id } }) => id);
 
         replies = [
           {
@@ -91,35 +91,171 @@ export default async function processMessages(
       break;
     }
     case 'CHOOSING_ARTICLE': {
-      if (!data.foundArticles) {
-        throw new Error('foundArticles not set in data');
+      if (!data.foundArticleIds) {
+        throw new Error('foundArticleIds not set in data');
       }
 
-      const selectedArticle = data.foundArticles[event.input - 1];
+      const selectedArticleId = data.foundArticleIds[event.input - 1];
 
-      if (!selectedArticle) {
+      if (!selectedArticleId) {
         replies = [
-          { type: 'text', text: `請輸入 1～${data.foundArticles.length} 的數字。` },
+          { type: 'text', text: `請輸入 1～${data.foundArticleIds.length} 的數字。` },
         ];
 
         state = 'CHOOSING_ARTICLE';
       } else {
-        // TODO: finish writing this.
-        replies = [
-          {
-            type: 'text',
-            text: '這則文章有 0 個回應表示查無不實， 0 個回應覺得有不實之處。',
+        const { data: { GetArticle } } = await gql`query($id: String!) {
+          GetArticle(id: $id) {
+            replies {
+              id
+              versions(limit: 1) {
+                type
+                text
+                reference
+                createdAt
+              }
+            }
+          }
+        }`({
+          id: selectedArticleId,
+        });
+
+        const { rumorReplies, notRumorReplies } = GetArticle.replies.reduce(
+          (result, reply) => {
+            if (reply.versions[0].type === 'RUMOR') {
+              result.rumorReplies.push(reply);
+            } else if (reply.versions[0].type === 'NOT_RUMOR') {
+              result.notRumorReplies.push(reply);
+            }
+            return result;
           },
-        ];
-        state = '__INIT__';
+          { rumorReplies: [], notRumorReplies: [] },
+        );
+
+        replies = [];
+        if (notRumorReplies.length) {
+          replies.push({ type: 'text', text: `這篇文章有 ${notRumorReplies.length} 個回應表示查無不實：` });
+          replies.push({
+            type: 'template',
+            altText: notRumorReplies.map(
+              ({ versions }, idx) => `閱讀請傳 ${idx + 1}> ${versions[0].text.slice(0, 20)}`,
+            ).join('\n\n'),
+            template: {
+              type: 'carousel',
+              columns: notRumorReplies.map(({ versions }, idx) => ({
+                text: versions[0].text.slice(0, 119),
+                actions: [
+                  createPostbackAction('閱讀此回應', idx + 1, issuedAt),
+                ],
+              })),
+            },
+          });
+        }
+        if (rumorReplies.length) {
+          replies.push({ type: 'text', text: `這篇文章有 ${rumorReplies.length} 個回應覺得有不實之處：` });
+          replies.push({
+            type: 'template',
+            altText: rumorReplies.map(
+              ({ versions }, idx) => `閱讀請傳 ${notRumorReplies.length + idx + 1}> ${versions[0].text.slice(0, 20)}`,
+            ).join('\n\n'),
+            template: {
+              type: 'carousel',
+              columns: rumorReplies.map(({ versions }, idx) => ({
+                text: versions[0].text.slice(0, 119),
+                actions: [
+                  createPostbackAction('閱讀此回應', notRumorReplies.length + idx + 1, issuedAt),
+                ],
+              })),
+            },
+          });
+        }
+
+        if (GetArticle.replies.length === 0) {
+          // No one has replied to this yet.
+          // TODO: Send replyRequest for the user.
+          //
+          replies = [
+            { type: 'text', text: '目前還沒有人回應這篇文章唷。' },
+            { type: 'text', text: `若有最新回應，會寫在這個地方：http://rumors.hacktabl.org/article/${data.selectedArticleId}` },
+          ];
+          state = '__INIT__';
+        } else if (replies.length === 0) {
+          // Someone reported that it is not an article,
+          // and there are no other replies available.
+          //
+          replies = [
+            { type: 'text', text: '有人認為您傳送的這則訊息並不是完整的文章內容，或認為「真的假的」不應該處理這則訊息。' },
+            { type: 'text', text: `詳情請見：http://rumors.hacktabl.org/article/${data.selectedArticleId}` },
+          ];
+          state = '__INIT__';
+        } else {
+          data.foundReplyIds = notRumorReplies.map(({ id }) => id)
+            .concat(rumorReplies.map(({ id }) => id));
+          state = 'CHOOSING_REPLY';
+        }
       }
 
       break;
     }
     case 'CHOOSING_REPLY': {
+      if (!data.foundReplyIds) {
+        throw new Error('foundReplyIds not set in data');
+      }
+
+      const selectedReplyId = data.foundReplyIds[event.input - 1];
+
+      if (!selectedReplyId) {
+        replies = [
+          { type: 'text', text: `請輸入 1～${data.foundReplyIds.length} 的數字。` },
+        ];
+
+        state = 'CHOOSING_REPLY';
+      } else {
+        const { data: { GetReply } } = await gql`query($id: String!) {
+          GetReply(id: $id) {
+            versions(limit: 1) {
+              type
+              text
+              reference
+              createdAt
+            }
+          }
+        }`({
+          id: selectedReplyId,
+        });
+
+        replies = [
+          { type: 'text', text: `這則回應認為文章${GetReply.versions[0].type === 'RUMOR' ? '含有不實訊息' : '不含不實訊息'}，理由為：` },
+          { type: 'text', text: GetReply.versions[0].text },
+          { type: 'text', text: `出處：${GetReply.versions[0].reference}` },
+          {
+            type: 'template',
+            altText: '請問這則回應是否有解答原文章？\n「是」請輸入「y」，「否」請輸入其他任何訊息。',
+            template: {
+              type: 'buttons',
+              text: '請問這則回應是否有解答原文章？',
+              actions: [
+                createPostbackAction('是', 'y', issuedAt),
+                createPostbackAction('否', 'n', issuedAt),
+              ],
+            },
+          },
+        ];
+
+        state = 'ASKING_REPLY_FEEDBACK';
+      }
+
       break;
     }
     case 'ASKING_REPLY_FEEDBACK': {
+      if (!data.foundReplyIds) {
+        throw new Error('foundReplyIds not set in data');
+      }
+      // TODO: send feedback
+      //
+      replies = [
+        { type: 'text', text: '感謝您的回饋。' },
+      ];
       break;
     }
     case 'ASKING_ARTICLE_SUBMISSION': {
@@ -156,6 +292,7 @@ export default async function processMessages(
         type: 'text',
         text: '我們看不懂 QQ\n大俠請重新來過。',
       }];
+      state = '__INIT__';
   }
 
   return {
