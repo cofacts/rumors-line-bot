fc4aae9d67986c2ae8798c5ff0b709756bcb4f83
changhc <changhc84@gmail.com>
2018-07-07 16:33:37 +0800

    asking not useful feedback: tests

---
+++ src/handlers/__fixtures__/askingNotUsefulFeedback.js
@@ -0,0 +1,15 @@
+export const oneFeedback = {
+  data: {
+    action: {
+      feedbackCount: 1,
+    },
+  },
+};
+
+export const twoFeedbacks = {
+  data: {
+    action: {
+      feedbackCount: 2,
+    },
+  },
+};
