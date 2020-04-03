989cc78c8f11fb9aa602b73c7f9193d0f4a386e2
ggm <godgunman@gmail.com>
2020-04-03 11:05:01 +0800

    Add schemaValidator

---
+++ src/database/models/schemaValidator.js
@@ -0,0 +1,43 @@
+import { ObjectId } from 'mongodb';
+import Ajv from 'ajv';
+
+const ajv = new Ajv();
+
+const SCHEMAS = {
+  userArticleLink: require('./userArticleLink.json'),
+  userSettings: require('./userSettings.json'),
+};
+
+const ClassTable = {
+  Date: Date,
+};
+
+ajv.addKeyword('instanceOf', {
+  compile: function(className) {
+    var Class = ClassTable[className];
+    return function(data) {
+      return data instanceof Class;
+    };
+  },
+});
+
+ajv.addKeyword('isObjectId', {
+  compile: function(isObjectId) {
+    return function(data) {
+      return isObjectId ? ObjectId.isValid(data) : true;
+    };
+  },
+});
+
+export function validate(schemaName, data) {
+  const valid = ajv.validate(SCHEMAS[schemaName], data);
+  return { valid, errors: ajv.errors };
+}
+
+export function compile(schemaName) {
+  return data => {
+    const validate = ajv.compile(SCHEMAS[schemaName]);
+    const valid = validate(data);
+    return { valid, errors: validate.errors };
+  };
+}
