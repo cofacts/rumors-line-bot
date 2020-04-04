30c8a3e52d0715a56eb6eace1484d834eb29cfe4
MrOrz <johnsonliang7@gmail.com>
2020-04-04 23:21:33 +0800

    Implement authentication, @auth directive and context field

---
+++ src/graphql/__tests__/context.js
@@ -0,0 +1,59 @@
+import { gql } from "../testUtils";
+
+it("context rejects anonymous users", async () => {
+  const result = await gql`
+    {
+      context {
+        state
+      }
+    }
+  `();
+  expect(result).toMatchInlineSnapshot(`
+    Object {
+      "data": Object {
+        "context": null,
+      },
+      "errors": Array [
+        [GraphQLError: Invalid authentication header],
+      ],
+    }
+  `);
+});
+
+it("Returns user context", async () => {
+  const result = await gql`
+    {
+      context {
+        state
+        issuedAt
+        data {
+          searchedText
+        }
+      }
+    }
+  `(
+    {},
+    {
+      userContext: {
+        state: "CHOOSING_ARTICLE",
+        issuedAt: 1586013070089,
+        data: {
+          searchedText: "Foo"
+        }
+      }
+    }
+  );
+  expect(result).toMatchInlineSnapshot(`
+    Object {
+      "data": Object {
+        "context": Object {
+          "data": Object {
+            "searchedText": "Foo",
+          },
+          "issuedAt": 1586013070089,
+          "state": "CHOOSING_ARTICLE",
+        },
+      },
+    }
+  `);
+});
