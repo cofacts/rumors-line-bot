ea856c552b8e8612936609b8fec6773c7a53d31d
MrOrz <johnsonliang7@gmail.com>
2020-04-05 16:26:39 +0800

    Remove submitArticle mutation because it's not needed

--- src/graphql/resolvers/Mutation.js
+++ src/graphql/resolvers/Mutation.js
@@ -1,33 +1,6 @@
 import gql from 'src/lib/gql';
 
 export default {
-  async submitArticle(root, args, context) {
-    const {
-      userContext: {
-        data: { searchedText },
-      },
-      userId,
-    } = context;
-
-    if (!searchedText) {
-      throw new Error('searchedText not in user context');
-    }
-
-    const {
-      data: {
-        CreateArticle: { id },
-      },
-    } = await gql`
-      mutation($text: String!, $reason: String!) {
-        CreateArticle(text: $text, reference: { type: LINE }) {
-          id
-        }
-      }
-    `({ text: searchedText }, { userId });
-
-    return id;
-  },
-
   async voteReply(root, args, context) {
     const { vote } = args;
     const {
