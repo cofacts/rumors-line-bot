ff62cbeaf422f6a2d593e9df0ceb4af5eac6d3e9
ggm <godgunman@gmail.com>
2020-04-08 23:44:21 +0800

    Add utility functions for model UserSettings

--- src/database/models/userSettings.js
+++ src/database/models/userSettings.js
@@ -4,6 +4,14 @@ import Base from './base';
  * https://g0v.hackmd.io/eIeU2g86Tfu5VnLazNfUvQ
  */
 class UserSettings extends Base {
+  static get DEFAULT_DATA() {
+    return {
+      createdAt: new Date(),
+      allowNewReplyUpdate: true,
+      newReplyNotifyToken: null,
+    };
+  }
+
   static get collection() {
     return 'userSettings';
   }
@@ -15,18 +23,57 @@ class UserSettings extends Base {
    * @returns {UserSettings}
    */
   static async create(data) {
-    // Set default
-    const {
-      allowNewReplyUpdate = true,
-      newReplyNotifyToken = null,
-      ...rest
-    } = data;
-
     return super.create({
-      ...rest,
-      allowNewReplyUpdate,
-      newReplyNotifyToken,
+      ...this.DEFAULT_DATA,
+      ...data,
     });
+  }
+
+  /**
+   * An atomic and upsert enabled operation.
+   * @param {string} userId
+   * @returns {Promise<UserSettings>}
+   */
+  static async findByUserId(userId) {
+    return this.findOneAndUpdate({ userId }, null, this.DEFAULT_DATA);
+  }
+
+  /**
+   * An atomic and upsert enabled operation.
+   * @param {string} userId
+   * @param {boolean} allow
+   * @returns {Promise<UserSettings>}
+   */
+  static async setAllowNewReplyUpdate(userId, allow) {
+    // eslint-disable-next-line no-unused-vars
+    const { allowNewReplyUpdate, ...$setOnInsert } = this.DEFAULT_DATA;
+
+    return this.findOneAndUpdate(
+      { userId },
+      {
+        allowNewReplyUpdate: allow,
+      },
+      $setOnInsert
+    );
+  }
+
+  /**
+   * An atomic and upsert enabled operation.
+   * @param {string} userId
+   * @param {string} token
+   * @returns {Promise<UserSettings>}
+   */
+  static async setNewReplyNotifyToken(userId, token) {
+    // eslint-disable-next-line no-unused-vars
+    const { newReplyNotifyToken, ...$setOnInsert } = this.DEFAULT_DATA;
+
+    return this.findOneAndUpdate(
+      { userId },
+      {
+        newReplyNotifyToken: token,
+      },
+      $setOnInsert
+    );
   }
 
   /**
@@ -38,6 +85,11 @@ class UserSettings extends Base {
    * @type {string}
    */
   userId;
+
+  /**
+   * @type {Date}
+   */
+  createdAt;
 
   /**
    * @type {boolean}
