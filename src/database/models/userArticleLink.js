52efdf236838f71ba09c6cafcd9a3d566d965d9f
ggm <godgunman@gmail.com>
2020-04-08 23:44:13 +0800

    Add utility functions for model UserArticleLink

--- src/database/models/userArticleLink.js
+++ src/database/models/userArticleLink.js
@@ -4,6 +4,15 @@ import Base from './base';
  * https://g0v.hackmd.io/eIeU2g86Tfu5VnLazNfUvQ
  */
 class UserArticleLink extends Base {
+  static get DEFAULT_DATA() {
+    return {
+      createdAt: new Date(),
+      lastViewedAt: null,
+      lastRepliedAt: null,
+      lastPositiveFeedbackRepliedAt: null,
+    };
+  }
+
   static get collection() {
     return 'userArticleLink';
   }
@@ -15,44 +24,77 @@ class UserArticleLink extends Base {
    * @returns {UserArticleLink}
    */
   static async create(data) {
-    // Set default value
-    const {
-      createdAt = new Date(),
-      lastViewedAt = null,
-      lastRepliedAt = null,
-      lastPositiveFeedbackRepliedAt = null,
-      ...rest
-    } = data;
-
     return super.create({
-      ...rest,
-      createdAt,
-      lastViewedAt,
-      lastRepliedAt,
-      lastPositiveFeedbackRepliedAt,
+      ...this.DEFAULT_DATA,
+      ...data,
     });
   }
 
   /**
+   * An atomic and upsert enabled operation.
+   * @param {string} userId
+   * @param {string} articleId
+   * @returns {Promise<UserArticleLink>}
+   */
+  static async findByUserIdAndArticleId(userId, articleId) {
+    return this.findOneAndUpdate(
+      { userId, articleId },
+      null,
+      this.DEFAULT_DATA
+    );
+  }
+
+  /**
+   * An atomic and upsert enabled operation.
+   * @typedef Timestamps
+   * @property {?Date} lastViewedAt
+   * @property {?Date} lastRepliedAt
+   * @property {?Date} lastPositiveFeedbackRepliedAt
+   *
+   * @param {string} userId
+   * @param {string} articleId
+   * @param {Timestamps} data
+   * @returns {Promise<UserArticleLink>}
+   */
+  static async updateTimestamps(userId, articleId, data) {
+    const setOnInsert = Object.assign({}, this.DEFAULT_DATA);
+
+    for (let key in data) {
+      delete setOnInsert[key];
+    }
+
+    return this.findOneAndUpdate({ userId, articleId }, data, setOnInsert);
+  }
+  /**
    * @type {?import('mongodb').ObjectId}
    */
   _id;
+
   /**
    * @type {string}
    */
   userId;
+
   /**
    * @type {string}
    */
   articleId;
+
+  /**
+   * @type {Date}
+   */
+  createdAt;
+
+  /**
+   * @type {?Date}
+   */
+  lastViewedAt;
+
   /**
    * @type {?Date}
    */
   lastRepliedAt;
-  /**
-   * @type {?Date}
-   */
-  lastRepliedAt;
+
   /**
    * @type {?Date}
    */
