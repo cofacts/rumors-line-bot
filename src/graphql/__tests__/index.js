29219be396abb9246a41df251895a8c5cbcf017e
MrOrz <johnsonliang7@gmail.com>
2020-04-26 20:22:49 +0800

    [graphql] JWT verification on GraphQL API

--- src/graphql/__tests__/index.js
+++ src/graphql/__tests__/index.js
@@ -1,6 +1,7 @@
 jest.mock('src/lib/redisClient');
 
 import { getContext } from '../';
+import { sign } from 'src/lib/jwt';
 import redis from 'src/lib/redisClient';
 
 beforeEach(() => {
@@ -14,49 +15,85 @@ describe('getContext', () => {
     expect(context).toMatchInlineSnapshot(`
       Object {
         "userContext": null,
-        "userId": "",
+        "userId": null,
       }
     `);
   });
 
   it('generates null context for wrong credentials', async () => {
     redis.get.mockImplementationOnce(() => ({
-      nonce: 'correctpass',
+      data: {
+        sessionId: 'correct-session-id',
+      },
     }));
 
-    const context = await getContext({
-      ctx: {
-        req: {
-          headers: {
-            authorization: `basic ${Buffer.from('user1:wrongpass').toString(
-              'base64'
-            )}`,
-          },
-        },
-      },
+    const jwtWithWrongSession = sign({
+      sessionId: 'wrong-session-id',
+      sub: 'user1',
+      exp: Date.now() / 1000 + 10, // future timestamp
     });
 
-    expect(context).toMatchInlineSnapshot(`
+    expect(
+      await getContext({
+        ctx: {
+          req: {
+            headers: {
+              authorization: `Bearer ${jwtWithWrongSession}`,
+            },
+          },
+        },
+      })
+    ).toMatchInlineSnapshot(`
       Object {
         "userContext": null,
         "userId": "user1",
+      }
+    `);
+
+    const jwtWithExpiredSession = sign({
+      sessionId: 'correct-session-id',
+      sub: 'user1',
+      exp: Date.now() / 1000 - 10, // past timestamp
+    });
+
+    // Expired JWTs will fail verification, thus its userId is not resolved either
+    expect(
+      await getContext({
+        ctx: {
+          req: {
+            headers: {
+              authorization: `Bearer ${jwtWithExpiredSession}`,
+            },
+          },
+        },
+      })
+    ).toMatchInlineSnapshot(`
+      Object {
+        "userContext": null,
+        "userId": null,
       }
     `);
   });
 
   it('reads userContext for logged-in requests', async () => {
     redis.get.mockImplementationOnce(() => ({
-      nonce: 'correctpass',
+      data: {
+        sessionId: 'correct-session-id',
+      },
       foo: 'bar',
     }));
+
+    const jwt = sign({
+      sessionId: 'correct-session-id',
+      sub: 'user1',
+      exp: Date.now() / 1000 + 10, // future timestamp
+    });
 
     const context = await getContext({
       ctx: {
         req: {
           headers: {
-            authorization: `basic ${Buffer.from('user1:correctpass').toString(
-              'base64'
-            )}`,
+            authorization: `Bearer ${jwt}`,
           },
         },
       },
@@ -65,8 +102,10 @@ describe('getContext', () => {
     expect(context).toMatchInlineSnapshot(`
       Object {
         "userContext": Object {
+          "data": Object {
+            "sessionId": "correct-session-id",
+          },
           "foo": "bar",
-          "nonce": "correctpass",
         },
         "userId": "user1",
       }
--- src/liff/lib.js
+++ src/liff/lib.js
@@ -19,3 +19,53 @@ export const token = params.get('token');
  * safe from XSS.
  */
 export const parsedToken = token ? JSON.parse(atob(token.split('.')[1])) : {};
+
+/**
+ * Usage: gql`query {...}`(variables)
+ *
+ * @returns {(variables: object): Promise<object>}
+ */
+export const gql = (query, ...substitutions) => variables => {
+  const queryAndVariable = {
+    query: String.raw(query, ...substitutions),
+  };
+
+  if (variables) queryAndVariable.variables = variables;
+
+  let status;
+
+  return fetch('/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(queryAndVariable),
+  })
+    .then(r => {
+      status = r.status;
+      return r.json();
+    })
+    .then(resp => {
+      if (status === 400) {
+        throw new Error(
+          `GraphQL Error: ${resp.errors
+            .map(({ message }) => message)
+            .join('\n')}`
+        );
+      }
+      if (resp.errors) {
+        // When status is 200 but have error, just print them out.
+        console.error('GraphQL operation contains error:', resp.errors);
+        rollbar.error(
+          'GraphQL error',
+          {
+            body: JSON.stringify(queryAndVariable),
+            url: URL,
+          },
+          { resp }
+        );
+      }
+      return resp;
+    });
+};
