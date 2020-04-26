d0657f4ccfa0d7d90ea785e94978447422af9582
MrOrz <johnsonliang7@gmail.com>
2020-04-26 22:21:55 +0800

    [liff] Test if gql() is working

--- src/liff/pages/Source.svelte
+++ src/liff/pages/Source.svelte
@@ -1,6 +1,23 @@
 <script>
+	import { onMount } from 'svelte';
   import { t } from 'ttag';
-  import { page } from '../lib';
+  import { page, gql } from '../lib';
+
+  let data = null;
+
+  // Demo purpose, will remove in next PR
+  onMount(() => {
+    gql`{
+      context {
+        state
+        data {
+          sessionId
+        }
+      }
+    }`().then(d => {
+      data = d;
+    })
+  })
 </script>
 
 <svelte:head>
@@ -8,6 +25,8 @@
 </svelte:head>
 
 <p>Source select</p>
+
+<pre>{JSON.stringify(data)}</pre>
 
 <!-- Fake; demonstrates how to navigate through pages -->
 <button on:click={() => page.set('reason')}>Go to reason</button>
