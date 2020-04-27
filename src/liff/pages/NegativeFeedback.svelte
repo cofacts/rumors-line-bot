d7a5e9561f1736a41bf04c3c09a953896cdc5172
MrOrz <johnsonliang7@gmail.com>
2020-04-28 01:37:50 +0800

    [liff] implement negative feedback LIFF

--- src/liff/pages/NegativeFeedback.svelte
+++ src/liff/pages/NegativeFeedback.svelte
@@ -1,9 +1,54 @@
 <script>
+  import { onMount } from 'svelte';
   import { t } from 'ttag';
+  import Button, { Label } from '@smui/button';
+  import Textfield, { Input, Textarea } from '@smui/textfield';
+  import { DOWNVOTE_PREFIX } from 'src/lib/sharedUtils';
+  import { gql } from '../lib';
+
+  let processing = false;
+  let comment = '';
+
+  // Submitting feedback without comment first
+  onMount(() => gql`
+    mutation VoteDown {
+      voteReply(vote: DOWNVOTE)
+    }
+  `());
+
+  const handleSubmit = async () => {
+    processing = true;
+
+    await liff.sendMessages([
+      {
+        type: 'text', text: `${DOWNVOTE_PREFIX}${comment}`,
+      }
+    ]);
+
+    processing = false;
+    liff.closeWindow();
+  }
 </script>
 
 <svelte:head>
-  <title>{t`Comment on the not-useful reply`}</title>
+  <title>{t`Report not useful`}</title>
 </svelte:head>
 
-<p>Negative Feedback</p>
+<p>{t`We are sorry that the reply is not useful to you.`}</p>
+
+<Textfield
+  fullwidth
+  textarea
+  bind:value={comment}
+  label={t`How can we make it useful to you?`}
+  input$rows={8}
+/>
+
+<Button
+  style="display: block; width: 100%; margin: 8px 0;"
+  variant="raised"
+  on:click={handleSubmit}
+  disabled={processing}
+>
+  <Label>{t`Submit`}</Label>
+</Button>
