d216a60682e1f2ff0601f57f57ecc616be863146
MrOrz <johnsonliang7@gmail.com>
2020-05-11 01:20:47 +0800

    [liff] init scaffold to support LINE token

--- src/liff/App.svelte
+++ src/liff/App.svelte
@@ -14,13 +14,21 @@
     'feedback/no': NegativeFeedback,
   };
 
-  const expired = (parsedToken.exp || -Infinity) < Date.now() / 1000;
+  const expired = parsedToken && (
+    (parsedToken.exp || -Infinity) < Date.now() / 1000
+  );
 
   // Chatbot context of current user
   let context = null;
 
   onMount(async () => {
     if(expired) return;
+
+    if(!parsedToken) {
+      // Set gql token to `line xxx` instead
+
+      return;
+    }
 
     const {data, errors} = await gql`
       query GetContextForLIFF {
