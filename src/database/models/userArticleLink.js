55967d3c44f9074ea1f2d79c0238c947b2bcf339
ggm <godgunman@gmail.com>
2020-06-18 03:16:05 +0800

    Implement UserArticleLink.findByUserId()

--- src/database/models/userArticleLink.js
+++ src/database/models/userArticleLink.js
@@ -63,6 +63,10 @@ class UserArticleLink extends Base {
     return this.findOneAndUpdate({ userId, articleId }, data, setOnInsert);
   }
 
+  static async findByUserId(userId) {
+    return (await this.client).find({ userId }).toArray();
+  }
+
   /**
    * @type {string}
    */
