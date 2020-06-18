bfaed639889fcb8913d97b4d0dfb7916c33a0815
MrOrz <johnsonliang7@gmail.com>
2020-06-18 17:33:59 +0800

    Initial setup for connection types

---
+++ src/graphql/resolvers/Cursor.js
@@ -0,0 +1,27 @@
+const { GraphQLScalarType, Kind } = require('graphql');
+
+function decodeToPaginationData(encoded) {
+  const value = +Buffer.from(encoded, 'base64').toString('utf8');
+  if (isNaN(value))
+    throw new Error(
+      'Should be a base64 string that can decode to pagination data'
+    );
+  return value;
+}
+
+export default new GraphQLScalarType({
+  name: 'Cursor',
+  parseLiteral(ast) {
+    if (ast.kind !== Kind.STRING) {
+      throw new Error('Should be a base64 string');
+    }
+
+    return decodeToPaginationData(ast.value);
+  },
+  parseValue(source) {
+    return decodeToPaginationData(source);
+  },
+  serialize(paginationData) {
+    return Buffer.from(paginationData.toString(), 'utf8').toString('base64');
+  },
+});
