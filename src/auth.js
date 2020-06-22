7648115633fbff4e82931a411cdcb944e89e1db1
Nonumpa <acerxp511@msn.com>
2020-06-22 16:55:01 +0800

    Implement line notify authorization

---
+++ src/auth.js
@@ -0,0 +1,104 @@
+import passport from 'koa-passport';
+import LineNotifyStrategy from './lib/passport-line-notify';
+import Router from 'koa-router';
+import url from 'url';
+import UserSettings from 'src/database/models/userSettings';
+
+/**
+ * Serialize to session
+ */
+passport.serializeUser((_, done) => {
+  done(null, _);
+});
+
+/**
+ * De-serialize and populates ctx.state.user
+ */
+passport.deserializeUser(async (_, done) => {
+  done(null, _);
+});
+
+if (process.env.LINE_NOTIFY_CLIENT_ID) {
+  passport.use(
+    new LineNotifyStrategy(
+      {
+        channelID: process.env.LINE_NOTIFY_CLIENT_ID,
+        channelSecret: process.env.LINE_NOTIFY_CLIENT_SECRET,
+        callbackURL: process.env.RUMORS_LINE_BOT_URL + '/authcallback/line_notify',
+      }, // line notify does not provide refreshToken and profile
+      (accessToken, refreshToken, profile, done) => {
+        // console.log('accseeToken:' + accessToken);
+        // no need to verifyProfile, just return accessToken
+        // done(err, user, info)
+        return done(null, accessToken);
+      }
+    )
+  );
+}
+
+// Exports route handlers
+//
+export const loginRouter = Router()
+  .use((ctx, next) => {
+    // Memorize redirect in session
+    //
+    if (!ctx.query.redirect || !ctx.query.redirect.startsWith('/')) {
+      const err = new Error(
+        '`redirect` must present in query string and start with `/`'
+      );
+      err.status = 400;
+      err.expose = true;
+      throw err;
+    }
+    if (!ctx.query.userId) {
+      const err = new Error('`userId` must present in query string');
+      err.status = 400;
+      err.expose = true;
+      throw err;
+    }
+    ctx.session.userId = ctx.query.userId;
+    ctx.session.redirect = ctx.query.redirect;
+
+    // execute next middleware (passport.authenticate('line_notify'))
+    next();
+  })
+  .get('/line_notify', passport.authenticate('line_notify'));
+
+const handlePassportCallback = strategy => (ctx, next) =>
+  passport.authenticate(strategy, (err, accessToken) => {
+    if (err) {
+      err.status = 401;
+      throw err;
+    }
+
+    // auth success, write token to db
+    // accessToken will be false when user cancel to auth
+    if (accessToken) {
+      UserSettings.setAllowNewReplyUpdate(ctx.session.userId, true);
+      UserSettings.setNewReplyNotifyToken(ctx.session.userId, accessToken);
+    }
+  })(ctx, next);
+
+export const authRouter = Router()
+  .use(async (ctx, next) => {
+    // Perform redirect after login
+    //
+    if (!ctx.session.redirect && !ctx.session.userId) {
+      const err = new Error(
+        '`userId` and `redirect` must be set before. Did you forget to go to /login/*?'
+      );
+      err.status = 400;
+      err.expose = true;
+      throw err;
+    }
+
+    // execute next middleware (handlePassportCallback('line_notify'))
+    await next();
+
+    ctx.redirect(
+      url.resolve(process.env.RUMORS_LINE_BOT_URL, ctx.session.redirect)
+    );
+    ctx.session.userId = undefined;
+    ctx.session.redirect = undefined;
+  })
+  .get('/line_notify', handlePassportCallback('line_notify'));
---
+++ src/lib/passport-line-notify/strategy.js
@@ -0,0 +1,85 @@
+/**
+ * Module dependencies.
+ */
+var util = require('util'),
+  OAuth2Strategy = require('passport-oauth2');
+
+/**
+ * `Strategy` constructor.
+ *
+ * https://notify-bot.line.me/doc/en/
+ * The LINE authentication strategy authenticates requests by delegating to
+ * LINE using the OAuth 2.0 protocol.
+ *
+ * Applications must supply a `verify` callback which accepts an `accessToken`,
+ * `refreshToken` and service-specific `profile`, and then calls the `done`
+ * callback supplying a `user`, which should be set to `false` if the
+ * credentials are not valid.  If an exception occured, `err` should be set.
+ *
+ * Options:
+ *   - `channelID`      your LINE channel's id
+ *   - `clientSecret`   your LINE channel's secret
+ *   - `callbackURL`   URL to which LINE will redirect the user after granting authorization
+ *
+ * Examples:
+ *
+ *     passport.use(new LineStrategy({
+ *         channelID: 'XXX',
+ *         clientSecret: 'XXXX'
+ *         callbackURL: 'https://www.example.net/auth/line/callback'
+ *       },
+ *       function(accessToken, refreshToken, profile, done) {
+ *         User.findOrCreate(..., function (err, user) {
+ *           done(err, user);
+ *         });
+ *       }
+ *     ));
+ *
+ * @param {Object} options
+ * @param {Function} verify
+ * @api public
+ */
+function Strategy(options, verify) {
+  if (options.state === false) {
+    throw new Error(
+      'options.state === false is not supported since LINE Login v2'
+    );
+  }
+
+  options = options || {};
+
+  // Default options.state to true (Now required by LINE v2)
+  // passport-oauth handles this and generates a unique state identification for the request
+  // This identification is then saved in Session (by default) and compared when response arrives
+  // NOTE: that now this requires you to use express-session (or a similar) session plugin
+  // Optionally you can use your own state store. See passport-oauth2 for more information.
+  options.state = options.state || true;
+
+  options.clientID = options.channelID;
+  options.clientSecret = options.channelSecret;
+  options.authorizationURL =
+    options.authorizationURL || 'https://notify-bot.line.me/oauth/authorize';
+  options.tokenURL =
+    options.tokenURL || 'https://notify-bot.line.me/oauth/token';
+
+  // accroding to line notify document, scope should be fixed value 'notify'
+  if (options.scope) console.error('scope should be fixed value "notify“');
+  else options.scope = 'notify';
+
+  OAuth2Strategy.call(this, options, verify);
+
+  this.name = 'line_notify';
+
+  // Use Authorization Header (Bearer with Access Token) for GET requests. Used to get User's profile.
+  this._oauth2.useAuthorizationHeaderforGET(true);
+}
+
+/**
+ * Inherit from `OAuth2Strategy`.
+ */
+util.inherits(Strategy, OAuth2Strategy);
+
+/**
+ * Expose `Strategy`.
+ */
+module.exports = Strategy;
