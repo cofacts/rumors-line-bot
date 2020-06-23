0db0a79a990f923794f0369124ce0fa736e898ed
Nonumpa <acerxp511@msn.com>
2020-06-24 01:37:30 +0800

    [liff] UserSetting use sumi/switch

--- src/liff/pages/UserSetting.svelte
+++ src/liff/pages/UserSetting.svelte
@@ -2,6 +2,7 @@
   import { onMount } from 'svelte';
   import { t } from 'ttag';
   import { gql } from '../lib';
+  import Switch from '@smui/switch';
 
   let methodName;
   let allowNewReplyUpdate;
@@ -71,73 +72,6 @@
     }
   }
 </script>
-<style>
-.box{
-  display: flex;
-  align-items: center;
-  margin: 8px 0;
-}
-
-.switch {
-  position: relative;
-  display: inline-block;
-  width: 60px;
-  height: 34px;
-}
-
-.switch input { 
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-
-.slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #ccc;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
-
-.slider:before {
-  position: absolute;
-  content: "";
-  height: 26px;
-  width: 26px;
-  left: 4px;
-  bottom: 4px;
-  background-color: white;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
-
-input:checked + .slider {
-  background-color: #2196F3;
-}
-
-input:focus + .slider {
-  box-shadow: 0 0 1px #2196F3;
-}
-
-input:checked + .slider:before {
-  -webkit-transform: translateX(26px);
-  -ms-transform: translateX(26px);
-  transform: translateX(26px);
-}
-
-/* Rounded sliders */
-.slider.round {
-  border-radius: 34px;
-}
-
-.slider.round:before {
-  border-radius: 50%;
-}
-</style>
 
 <svelte:head>
   <title>{t`Settings`}</title>
@@ -146,16 +80,14 @@ input:checked + .slider:before {
 <p>{t`Notification`}</p>
 
 <!-- https://stackoverflow.com/questions/59027947/how-to-have-a-conditional-attribute-in-svelte-3 -->
-<label class="box">
+<div style="display: flex;">
   {#if methodName}
-    <span style="width: calc(100% - 60px);">{methodName}</span>
-    <label class="switch">
-      <input type="checkbox"
-      on:click={(event) => handleClick(event)}
-      checked={allowNewReplyUpdate || undefined}>
-      <span class="slider round"></span>
-  </label>
+      <span style="width: calc(100% - 60px);">{methodName}</span>
+      <Switch
+      style="margin: 0 10px;"
+      on:click={(event) => handleClick(event)} 
+      bind:checked={allowNewReplyUpdate} />
   {:else}
     <span>{t`Not available`}</span>
   {/if}
-</label>
+</div>
