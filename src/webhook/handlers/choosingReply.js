d1702b645594f8fd1594e6ad19e51136b1f616a0
MrOrz <johnsonliang7@gmail.com>
2020-10-12 00:26:43 +0800

    [choosingReply] add notification setting to reply's reply

--- src/webhook/handlers/choosingReply.js
+++ src/webhook/handlers/choosingReply.js
@@ -8,8 +8,10 @@ import {
   getLIFFURL,
   ManipulationError,
   FLEX_MESSAGE_ALT_TEXT,
+  createNotificationSettingsBubble,
 } from './utils';
 import ga from 'src/lib/ga';
+import UserSettings from 'src/database/models/userSettings';
 
 export default async function choosingReply(params) {
   let { data, state, event, issuedAt, userId, replies, isSkipUser } = params;
@@ -45,6 +47,9 @@ export default async function choosingReply(params) {
   const articleUrl = getArticleURL(data.selectedArticleId);
   const typeStr = createTypeWords(GetReply.type).toLocaleLowerCase();
   const helpfulTitle = t`Is the reply helpful?`;
+  const { allowNewReplyUpdate } = await UserSettings.findOrInsertByUserId(
+    userId
+  );
 
   replies = [
     {
@@ -73,47 +78,56 @@ export default async function choosingReply(params) {
       type: 'flex',
       altText: helpfulTitle + '\n' + FLEX_MESSAGE_ALT_TEXT,
       contents: {
-        type: 'bubble',
-        body: {
-          type: 'box',
-          layout: 'vertical',
-          spacing: 'md',
-          paddingAll: 'lg',
-          contents: [
-            {
-              type: 'text',
-              wrap: true,
-              text: helpfulTitle,
+        type: 'carousel',
+        contents: [
+          {
+            type: 'bubble',
+            body: {
+              type: 'box',
+              layout: 'vertical',
+              paddingAll: 'xl',
+              contents: [
+                {
+                  type: 'text',
+                  wrap: true,
+                  text: helpfulTitle,
+                },
+              ],
             },
-          ],
-        },
-        footer: {
-          type: 'box',
-          layout: 'horizontal',
-          spacing: 'sm',
-          contents: [
-            {
-              type: 'button',
-              style: 'primary',
-              color: '#ffb600',
-              action: {
-                type: 'uri',
-                label: t`Yes`,
-                uri: getLIFFURL('feedback/yes', userId, data.sessionId),
-              },
+            footer: {
+              type: 'box',
+              layout: 'horizontal',
+              spacing: 'sm',
+              contents: [
+                {
+                  type: 'button',
+                  style: 'primary',
+                  color: '#ffb600',
+                  action: {
+                    type: 'uri',
+                    label: t`Yes`,
+                    uri: getLIFFURL('feedback/yes', userId, data.sessionId),
+                  },
+                },
+                {
+                  type: 'button',
+                  style: 'primary',
+                  color: '#ffb600',
+                  action: {
+                    type: 'uri',
+                    label: t`No`,
+                    uri: getLIFFURL('feedback/no', userId, data.sessionId),
+                  },
+                },
+              ],
             },
-            {
-              type: 'button',
-              style: 'primary',
-              color: '#ffb600',
-              action: {
-                type: 'uri',
-                label: t`No`,
-                uri: getLIFFURL('feedback/no', userId, data.sessionId),
-              },
-            },
-          ],
-        },
+          },
+          // Ask user to turn on notification if the user did not turn it on
+          //
+          process.env.NOTIFY_METHOD &&
+            !allowNewReplyUpdate &&
+            createNotificationSettingsBubble(),
+        ].filter(m => m),
       },
     },
   ];
