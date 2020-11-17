ef8b5db4e9811639a7882c6579ef423504360369
Nonumpa <acerxp511@msn.com>
2020-11-17 12:05:14 +0800

    Test dialogflow

---
+++ src/lib/__tests__/detectDialogflowIntent.js
@@ -0,0 +1,168 @@
+const mockSessionPath = jest.fn();
+const mockDetectIntent = jest.fn();
+
+let detectDialogflowIntent;
+beforeEach(() => {
+  // following variables are just for `detectDialogflowIntent` env varialbe check
+  process.env.DAILOGFLOW_PROJECT_ID = 'projectId';
+  process.env.DAILOGFLOW_CLIENT_EMAIL = 'client_email';
+  process.env.DAILOGFLOW_PRIVATE_KEY = 'private_key';
+
+  mockSessionPath.mockClear();
+  mockDetectIntent.mockClear();
+
+  // To test different case of process.env.* in `detectDialogflowIntent` we should resetModules
+  //
+  jest.resetModules();
+  jest.mock('@google-cloud/dialogflow', () => ({
+    SessionsClient: jest.fn().mockImplementation(() => ({
+      projectAgentEnvironmentUserSessionPath: mockSessionPath,
+      detectIntent: mockDetectIntent,
+    })),
+  }));
+});
+
+afterEach(() => {
+  delete process.env.DAILOGFLOW_PROJECT_ID;
+  delete process.env.DAILOGFLOW_CLIENT_EMAIL;
+  delete process.env.DAILOGFLOW_PRIVATE_KEY;
+});
+
+const intentResponse = [
+  {
+    responseId: 'response_Id',
+    queryResult: {
+      fulfillmentMessages: [
+        {
+          platform: 'PLATFORM_UNSPECIFIED',
+          text: { text: ['Hey！'] },
+          message: 'text',
+        },
+      ],
+      outputContexts: [],
+      queryText: 'Hi',
+      speechRecognitionConfidence: 0,
+      action: 'input.welcome',
+      parameters: { fields: {} },
+      allRequiredParamsPresent: true,
+      fulfillmentText: 'Hey！',
+      webhookSource: '',
+      webhookPayload: null,
+      intent: {
+        inputContextNames: [],
+        events: [],
+        trainingPhrases: [],
+        outputContexts: [],
+        parameters: [],
+        messages: [],
+        defaultResponsePlatforms: [],
+        followupIntentInfo: [],
+        name: 'projects/project_id/agent/intents/intent_id',
+        displayName: 'Default Welcome Intent',
+        priority: 0,
+        isFallback: false,
+        webhookState: 'WEBHOOK_STATE_UNSPECIFIED',
+        action: '',
+        resetContexts: false,
+        rootFollowupIntentName: '',
+        parentFollowupIntentName: '',
+        mlDisabled: false,
+      },
+      intentDetectionConfidence: 1,
+      diagnosticInfo: null,
+      languageCode: 'zh-tw',
+      sentimentAnalysisResult: null,
+    },
+    webhookStatus: null,
+    outputAudio: { type: 'Buffer', data: [] },
+    outputAudioConfig: null,
+  },
+];
+
+it('skip detecting intent', async () => {
+  delete process.env.DAILOGFLOW_CLIENT_EMAIL;
+  detectDialogflowIntent = require('../detectDialogflowIntent').default;
+  mockDetectIntent.mockImplementation(() => intentResponse);
+  expect(await detectDialogflowIntent('Hi')).toMatchInlineSnapshot(`undefined`);
+  expect(mockSessionPath).not.toHaveBeenCalled();
+  expect(mockDetectIntent).not.toHaveBeenCalled();
+});
+
+it('detects intent', async () => {
+  detectDialogflowIntent = require('../detectDialogflowIntent').default;
+  mockDetectIntent.mockImplementation(() => intentResponse);
+  expect(await detectDialogflowIntent('Hi')).toMatchInlineSnapshot(`
+    Object {
+      "outputAudio": Object {
+        "data": Array [],
+        "type": "Buffer",
+      },
+      "outputAudioConfig": null,
+      "queryResult": Object {
+        "action": "input.welcome",
+        "allRequiredParamsPresent": true,
+        "diagnosticInfo": null,
+        "fulfillmentMessages": Array [
+          Object {
+            "message": "text",
+            "platform": "PLATFORM_UNSPECIFIED",
+            "text": Object {
+              "text": Array [
+                "Hey！",
+              ],
+            },
+          },
+        ],
+        "fulfillmentText": "Hey！",
+        "intent": Object {
+          "action": "",
+          "defaultResponsePlatforms": Array [],
+          "displayName": "Default Welcome Intent",
+          "events": Array [],
+          "followupIntentInfo": Array [],
+          "inputContextNames": Array [],
+          "isFallback": false,
+          "messages": Array [],
+          "mlDisabled": false,
+          "name": "projects/project_id/agent/intents/intent_id",
+          "outputContexts": Array [],
+          "parameters": Array [],
+          "parentFollowupIntentName": "",
+          "priority": 0,
+          "resetContexts": false,
+          "rootFollowupIntentName": "",
+          "trainingPhrases": Array [],
+          "webhookState": "WEBHOOK_STATE_UNSPECIFIED",
+        },
+        "intentDetectionConfidence": 1,
+        "languageCode": "zh-tw",
+        "outputContexts": Array [],
+        "parameters": Object {
+          "fields": Object {},
+        },
+        "queryText": "Hi",
+        "sentimentAnalysisResult": null,
+        "speechRecognitionConfidence": 0,
+        "webhookPayload": null,
+        "webhookSource": "",
+      },
+      "responseId": "response_Id",
+      "webhookStatus": null,
+    }
+  `);
+  expect(mockSessionPath).toHaveBeenCalledTimes(1);
+  expect(mockDetectIntent).toHaveBeenCalledTimes(1);
+});
+
+it('handles error', async () => {
+  detectDialogflowIntent = require('../detectDialogflowIntent').default;
+  mockDetectIntent.mockImplementation(() => {
+    const error = new Error(
+      `3 INVALID_ARGUMENT: Resource name 'projects/undefined_project_id/agent/sessions/sessionId/' does not match 'projects/*/locations/*/agent/environments/*/users/*/sessions/*'.`
+    );
+    throw error;
+  });
+  expect(await detectDialogflowIntent('Hi')).toMatchInlineSnapshot(`undefined`);
+  expect(mockSessionPath).toHaveBeenCalledTimes(1);
+  expect(mockDetectIntent).toHaveBeenCalledTimes(1);
+});
