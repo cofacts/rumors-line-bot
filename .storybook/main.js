f580972ec30f14af691a69d05ef6752573b89302
MrOrz <johnsonliang7@gmail.com>
2021-06-13 00:29:30 +0800

    [storybook] disable unused options

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,13 @@ module.exports = {
   ],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        backgrounds: false,
+        toolbars: false,
+      },
+    },
     '@storybook/addon-svelte-csf',
   ],
   webpackFinal: async config => {
