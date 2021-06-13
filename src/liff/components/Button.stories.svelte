3ffed24d7c07da22098bdacd096d12f48c700071
MrOrz <johnsonliang7@gmail.com>
2021-06-14 01:18:00 +0800

    Initial implementation of Button

---
+++ src/liff/components/Button.stories.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import Button from "./Button.svelte";
+</script>
+
+<Meta
+  title="Button"
+  component={Button}
+  argTypes={{
+    onClick: {action: 'onClick'},
+  }}
+/>
+
+<Template let:args>
+  <Button {...args} on:click={args.onClick}>
+    Button text
+  </Button>
+</Template>
+
+<Story
+  name="Empty button"
+/>
+
+<Story name="Variants">
+  <Button variant="primary">variant = primary</Button>
+  <Button variant="outlined">variant = outlined</Button>
+</Story>
+
+<Story name="Passed props"
+  args={{
+    disabled: true,
+  }}
+/>
---
+++ src/liff/components/Button.svelte
@@ -0,0 +1,34 @@
+<script>
+  import clsx from 'clsx';
+
+  // primary || outlined
+  export let variant = 'outlined';
+</script>
+
+<style>
+  .btn {
+    appearance: none;
+    padding: 8px;
+  }
+
+  .outlined {
+    background: transparent;
+    border: 2px solid currentColor;
+  }
+
+  .outlined:hover {
+    background: rgba()
+  }
+
+  .primary {
+    background: #fff;
+  }
+
+  .btn:disabled {
+    pointer-events: none;
+  }
+</style>
+
+<button class={clsx('btn', variant)} {...$$restProps}>
+  <slot />
+</button>
