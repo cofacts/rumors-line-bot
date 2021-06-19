38b4b5f43287af1a59a9a32cd648fe727faedc2e
MrOrz <johnsonliang7@gmail.com>
2021-06-19 22:10:25 +0800

    TextArea component

---
+++ src/liff/components/TextArea.svelte
@@ -0,0 +1,22 @@
+<style>
+  textarea {
+    border: 0;
+    padding: 8px;
+    background: var(--secondary50);
+    color: var(--secondary500);
+    transition: background-color 0.2s;
+    font-size: inherit;
+    border-radius: 8px;
+  }
+
+  textarea::placeholder {
+    color: var(--secondary200);
+  }
+
+  textarea:focus {
+    background: #fff;
+    outline: 0;
+  }
+</style>
+
+<textarea {...$$restProps} />
