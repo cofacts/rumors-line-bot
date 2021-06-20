ee68d2fc64afbb1a26c144be243ce783e330ba9b
MrOrz <johnsonliang7@gmail.com>
2021-06-20 23:44:05 +0800

    Remove smui from UserSetting

--- src/liff/pages/UserSetting.svelte
+++ src/liff/pages/UserSetting.svelte
@@ -2,9 +2,7 @@
   import { onMount } from 'svelte';
   import { t } from 'ttag';
   import { gql } from '../lib';
-  import Switch from '@smui/switch';
-  import Paper, {Title, Content} from '@smui/paper';
-  import FormField from '@smui/form-field';
+  import Switch from '../components/Switch';
 
   /* Expose notify method to UI */
   const notifyMethod = NOTIFY_METHOD;
@@ -64,13 +62,15 @@
   }
 </script>
 <style>
-  :global(.field) {
+  .control {
     display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
 
-  :global(.field.field.field.field > label) {
-    padding: 0;
-    margin-right: auto;
+  .card {
+    background: #fff;
+    padding: 16px;
   }
 </style>
 
@@ -78,27 +78,29 @@
   <title>{t`Settings`}</title>
 </svelte:head>
 
-<Paper color="primary" class="paper-demo">
-  <Title>{t`Welcome to Cofacts!`}</Title>
-  <Content>{t`You can configure Cofacts here to meet your need.`}</Content>
-</Paper>
+<header>
+  <h1>{t`Welcome to Cofacts!`}</h1>
+  <p>{t`You can configure Cofacts here to meet your need.`}</p>
+</header>
 
 {#if notifyMethod}
   {#if isLoadingData}
     <p>{t`Fetching settings`}...</p>
   {:else}
-    <p>{t`Cofacts can send you latest reply of messages you have sent to Cofacts before.`}</p>
-    <FormField class="field" align="end">
-      <span slot="label">
+    <div class="card">
+      <div class="control">
         {t`Notify me of new responses`}
-      </span>
-      <Switch
-        style="margin: 0 10px;"
-        bind:checked={allowNewReplyUpdate}
-        on:change={(event) => handleClick(event)}
-        disabled={allowNewReplyUpdate === undefined}
-      />
-    </FormField>
+
+        <Switch
+          bind:checked={allowNewReplyUpdate}
+          on:change={(event) => handleClick(event)}
+          disabled={allowNewReplyUpdate === undefined}
+        />
+      </div>
+      <p style="margin-bottom: 0">
+        {t`Cofacts can send you latest reply of messages you have sent to Cofacts before.`}
+      </p>
+    </div>
   {/if}
 {:else}
   <p>{t`No setup option for now :)`}</p>
