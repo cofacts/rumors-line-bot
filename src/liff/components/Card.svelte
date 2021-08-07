e5cca1d7fa0bec0f915dc8a94b47cd7a7f11fa57
MrOrz <johnsonliang7@gmail.com>
2021-08-07 17:20:16 +0800

    [Card] Implement card with customizable gap & colors

---
+++ src/liff/components/Card.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let style;
+</script>
+
+<style>
+  .card {
+    background: var(--background, #fff);
+    color: var(--color, var(--secondary500));
+
+    padding: 16px;
+
+    /* For iOS devices < 14.7, which does not support flexbox gap... */
+    display: grid;
+    grid-auto-flow: row;
+
+    /* Introduces "--gap" variable */
+    row-gap: var(--gap, 16px);
+  }
+</style>
+
+<div class="card" {style}>
+  <slot />
+</div>
