43ac6f4018ca367e149b0d59ed99490034793e6a
MrOrz <johnsonliang7@gmail.com>
2021-08-11 01:33:44 +0800

    Stories for FullpagePrompt, Header & Spacer

---
+++ src/liff/components/Header.stories.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Meta, Story } from "@storybook/addon-svelte-csf";
+  import Header from "./Header.svelte";
+  import { argsToStyle } from "./storiesUtils"
+</script>
+
+<Meta
+  title="Header"
+  component={Header}
+  args={{
+    text: 'Section header text here'
+  }}
+/>
+
+<Story name="Default" let:args>
+  <Header>
+    {args.text}
+  </Header>
+</Story>
--- src/liff/components/Spacer.svelte
+++ src/liff/components/Spacer.svelte
@@ -6,19 +6,19 @@
     display: flex;
     justify-content: center;
     color: var(--secondary100);
-    --default-dot-size: 4px;
-    padding: var(--dot-size, var(--default-dot-size));
+    --dot-size: 4px;
+    padding: var(--dot-size);
   }
 
   div::before {
     content: '';
-    width: var(--dot-size, var(--default-dot-size));
-    height: var(--dot-size, var(--default-dot-size));
-    border-radius: var(--dot-size, var(--default-dot-size));
+    width: var(--dot-size);
+    height: var(--dot-size);
+    border-radius: var(--dot-size);
     background: currentColor;
 
-    box-shadow: calc(2 * var(--dot-size, var(--default-dot-size))) 0 currentColor,
-      calc(-2 * var(--dot-size, var(--default-dot-size))) 0 currentColor;
+    box-shadow: calc(2 * var(--dot-size)) 0 currentColor,
+      calc(-2 * var(--dot-size)) 0 currentColor;
   }
 </style>
 <div {style} />
