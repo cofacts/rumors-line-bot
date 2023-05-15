c61064924f2b4107c945e517ab28d5f80e46630a
Johnson Liang <johnsonliang7@gmail.com>
2023-05-15 13:24:24 +0800

    fix(ga): fix ga mock by renaming to ts - Also adjusts ga() initialization to make it output correct type

---
+++ src/lib/__mocks__/ga.ts
@@ -0,0 +1,25 @@
+const eventMock = jest.fn();
+const sendMock = jest.fn();
+const setMock = jest.fn();
+
+const ga = Object.assign(
+  jest.fn().mockImplementation(() => ({
+    event: eventMock,
+    send: sendMock,
+    set: setMock,
+    screenview: () => {},
+  })),
+  {
+    clearAllMocks: () => {
+      eventMock.mockClear();
+      sendMock.mockClear();
+      setMock.mockClear();
+      ga.mockClear();
+    },
+    eventMock,
+    sendMock,
+    setMock
+  }
+);
+
+export default ga;
