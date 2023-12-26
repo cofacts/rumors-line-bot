cf404b3481a3b5fc00057a47ed8100fa516c3662
MrOrz <johnsonliang7@gmail.com>
2023-12-27 02:05:25 +0800

    feat(webhook): implement "no" on askingCooccurrences

--- src/webhook/handlers/askingCooccurrence.ts
+++ src/webhook/handlers/askingCooccurrence.ts
@@ -4,7 +4,12 @@ import { t } from 'ttag';
 import ga from 'src/lib/ga';
 import { ChatbotPostbackHandler } from 'src/types/chatbotState';
 
-import { POSTBACK_YES, POSTBACK_NO, ManipulationError } from './utils';
+import {
+  POSTBACK_YES,
+  POSTBACK_NO,
+  ManipulationError,
+  createTextMessage,
+} from './utils';
 
 const inputSchema = z.enum([POSTBACK_NO, POSTBACK_YES]);
 
@@ -24,10 +29,45 @@ const askingCooccurence: ChatbotPostbackHandler = async ({
     throw new ManipulationError(t`Please choose from provided options.`);
   }
 
-  return {
-    context,
-    replies: [],
-  };
+  const visitor = ga(userId, state, `Batch: ${context.msgs.length} messages`);
+
+  switch (input) {
+    case POSTBACK_NO: {
+      visitor
+        .event({
+          ec: 'UserInput',
+          ea: 'IsCooccurrence',
+          el: 'No',
+        })
+        .send();
+      return {
+        context,
+        replies: [
+          createTextMessage({
+            text: t`Please send me the messages separately.`,
+          }),
+        ],
+      };
+    }
+
+    case POSTBACK_YES: {
+      visitor
+        .event({
+          ec: 'UserInput',
+          ea: 'IsCooccurrence',
+          el: 'Yes',
+        })
+        .send();
+      return {
+        context,
+        replies: [],
+      };
+    }
+
+    default:
+      // exhaustive check
+      return input satisfies never;
+  }
 };
 
 export default askingCooccurence;
