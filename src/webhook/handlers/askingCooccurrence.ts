1b6fa3b220364d88a20c49bc29c504ab3469e301
MrOrz <johnsonliang7@gmail.com>
2024-01-02 01:58:23 +0800

    feat(handlers): adding search and set occurrence utility

--- src/webhook/handlers/askingCooccurrence.ts
+++ src/webhook/handlers/askingCooccurrence.ts
@@ -9,12 +9,29 @@ import {
   POSTBACK_NO,
   ManipulationError,
   createTextMessage,
+  searchText,
+  searchMedia,
+  getLineContentProxyURL,
 } from './utils';
+import gql from 'src/lib/gql';
 
 const inputSchema = z.enum([POSTBACK_NO, POSTBACK_YES]);
 
+/** Minimum similarity for a CooccurredMessage to be considered as existing in DB */
+const IN_DB_THRESHOLD = 0.8;
+
 /** Postback input type for ASKING_ARTICLE_SOURCE state handler */
 export type Input = z.infer<typeof inputSchema>;
+
+export function setCooccurrences(articleIds: string[]) {
+  return gql`
+    mutation SetCooccurrences($articleIds: [String!]!) {
+      CreateOrUpdateCooccurrence(articleIds: $articleIds) {
+        id
+      }
+    }
+  `({ articleIds });
+}
 
 const askingCooccurence: ChatbotPostbackHandler = async ({
   context,
@@ -58,8 +75,38 @@ const askingCooccurence: ChatbotPostbackHandler = async ({
           el: 'Yes',
         })
         .send();
+
+      const searchResults = await Promise.all(
+        context.msgs.map(async (msg) =>
+          msg.type === 'text'
+            ? searchText(msg.text)
+            : searchMedia(getLineContentProxyURL(msg.id), userId)
+        )
+      );
+
+      const notInDbCount = searchResults.reduce((sum, result) => {
+        const firstResult = result.edges[0];
+        if (!firstResult) return sum + 1;
+
+        return ('mediaSimilarity' in firstResult
+          ? firstResult.mediaSimilarity
+          : firstResult.similarity) >= IN_DB_THRESHOLD
+          ? sum
+          : sum + 1;
+      }, 0);
+
+      if (notInDbCount === 0) {
+        // Get first few search results for each message, and make at most 10 options
+
+        return {
+          context,
+          replies: [],
+        };
+      }
+
       return {
         context,
+        // Ask if the user want to submit those are not in DB into the database
         replies: [],
       };
     }
