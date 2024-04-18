dcbaf241914b5f6f1ab3befb252031e8a8968037
Johnson Liang <johnsonliang7@gmail.com>
2024-04-18 13:48:34 +0800

    fix: typo in imports

--- src/graphql/resolvers/Query.js
+++ src/graphql/resolvers/Query.js
@@ -1,6 +1,6 @@
 import UserArticleLink from 'src/database/models/userArticleLink';
 import UserSettings from 'src/database/models/userSettings';
-import AppVariable from 'src/database/models/AppVariable';
+import AppVariable from 'src/database/models/appVariable';
 import { groupEventQueue, expiredGroupEventQueue } from 'src/lib/queues';
 import { processConnection } from '../utils/connection';
 
