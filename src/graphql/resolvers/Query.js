c29d97bcbb7923d3a1e2446b170840515b8acb9c
Johnson Liang <johnsonliang7@gmail.com>
2024-04-18 13:24:37 +0800

    feat: add awardee check to mgpAwardee

--- src/graphql/resolvers/Query.js
+++ src/graphql/resolvers/Query.js
@@ -1,5 +1,6 @@
 import UserArticleLink from 'src/database/models/userArticleLink';
 import UserSettings from 'src/database/models/userSettings';
+import AppVariable from 'src/database/models/AppVariable';
 import { groupEventQueue, expiredGroupEventQueue } from 'src/lib/queues';
 import { processConnection } from '../utils/connection';
 
@@ -25,5 +26,12 @@ export default {
       filter: { userId },
       orderBy,
     });
+  },
+
+  async isMgpAwardee(root, args, { userId }) {
+    const awardees = ((await AppVariable.get('mgpAwardees')) || '')
+      .trim()
+      .split('\n');
+    return awardees.includes(userId);
   },
 };
