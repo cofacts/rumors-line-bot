ea6609f6e0fdc906528028b2fae2bd4fa73b3d51
MrOrz <johnsonliang7@gmail.com>
2024-09-16 04:05:24 +0800

    test(webhook): add test for askingCooccurrence when all in DB

--- src/webhook/handlers/__tests__/askingCooccurrence.test.ts
+++ src/webhook/handlers/__tests__/askingCooccurrence.test.ts
@@ -7,7 +7,14 @@ import askingCooccurrence, { Input } from '../askingCooccurrence';
 import originalGql from 'src/lib/gql';
 import originalGa from 'src/lib/ga';
 import { ChatbotPostbackHandlerParams } from 'src/types/chatbotState';
-import { POSTBACK_NO } from '../utils';
+import { POSTBACK_NO, POSTBACK_YES } from '../utils';
+import {
+  AddReplyRequestForUnrepliedArticleMutation,
+  ListArticlesInInitStateQuery,
+  ListArticlesInProcessMediaQuery,
+  SetCooccurrencesMutation,
+} from 'typegen/graphql';
+import { imageMessage, textMessage } from '../__fixtures__/askingCooccurrence';
 
 const ga = originalGa as MockedGa;
 const gql = originalGql as MockedGql;
@@ -85,5 +92,211 @@ it('tells user to send messages separately', async () => {
   `);
 });
 
-// it('stores cooccurrences and reply requests as expected', async () => {
-// });
+it('stores cooccurrences whose article are all in DB', async () => {
+  const params: ChatbotPostbackHandlerParams<Input> = {
+    context: {
+      sessionId: 0,
+      msgs: [
+        // Triggers searchText
+        { type: 'text', text: textMessage.text, id: 'txt-message' },
+        // Triggers searchMedia
+        {
+          type: 'image',
+          id: 'img-message',
+        },
+      ],
+    },
+    postbackData: {
+      sessionId: 0,
+      state: 'ASKING_COOCCURRENCE',
+      input: POSTBACK_YES,
+    },
+    userId: 'userId',
+  };
+
+  // Prepare gql mock by the order of the queries/mutations
+  //
+  [
+    // searchText
+    {
+      ListArticles: {
+        edges: [{ node: textMessage, highlight: null }],
+      },
+    } satisfies ListArticlesInInitStateQuery,
+
+    // searchMedia
+    {
+      ListArticles: {
+        edges: [
+          { node: imageMessage, score: 2, mediaSimilarity: 1, highlight: null },
+        ],
+      },
+    } satisfies ListArticlesInProcessMediaQuery,
+
+    // setMostSimilarArticlesAsCooccurrence
+    {
+      CreateOrUpdateCooccurrence: { id: 'some-id' },
+    } satisfies SetCooccurrencesMutation,
+
+    // addReplyRequestForUnrepliedCooccurredArticles
+    {
+      CreateOrUpdateReplyRequest: { id: 'some-id-1' },
+    } satisfies AddReplyRequestForUnrepliedArticleMutation,
+
+    {
+      CreateOrUpdateReplyRequest: { id: 'some-id-2' },
+    } satisfies AddReplyRequestForUnrepliedArticleMutation,
+  ].forEach((resp) => gql.__push({ data: resp }));
+
+  const { replies } = await askingCooccurrence(params);
+  expect(gql.__finished()).toBe(true);
+  expect(ga.eventMock.mock.calls).toMatchInlineSnapshot(`
+    Array [
+      Array [
+        Object {
+          "ea": "IsCooccurrence",
+          "ec": "UserInput",
+          "el": "Yes",
+        },
+      ],
+    ]
+  `);
+
+  // Should list both messages in occurrences
+  //
+  expect(replies).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "altText": "🔍 There are some messages that looks similar to the ones you have sent to me.",
+        "contents": Object {
+          "body": Object {
+            "contents": Array [
+              Object {
+                "text": "🔍 There are some messages that looks similar to the ones you have sent to me.",
+                "type": "text",
+                "wrap": true,
+              },
+            ],
+            "layout": "vertical",
+            "type": "box",
+          },
+          "type": "bubble",
+        },
+        "type": "flex",
+      },
+      Object {
+        "altText": "Internet rumors are often mutated and shared.
+    Please choose the version that looks the most similar👇",
+        "contents": Object {
+          "body": Object {
+            "contents": Array [
+              Object {
+                "text": "Internet rumors are often mutated and shared.
+    Please choose the version that looks the most similar👇",
+                "type": "text",
+                "wrap": true,
+              },
+            ],
+            "layout": "vertical",
+            "type": "box",
+          },
+          "type": "bubble",
+        },
+        "type": "flex",
+      },
+      Object {
+        "altText": "Please choose the most similar message from the list.",
+        "contents": Object {
+          "contents": Array [
+            Object {
+              "body": undefined,
+              "direction": "ltr",
+              "footer": Object {
+                "contents": Array [
+                  Object {
+                    "action": Object {
+                      "data": "{\\"input\\":\\"text-message\\",\\"sessionId\\":0,\\"state\\":\\"CHOOSING_ARTICLE\\"}",
+                      "displayText": "I choose ①",
+                      "label": "Choose this one",
+                      "type": "postback",
+                    },
+                    "color": "#ffb600",
+                    "style": "primary",
+                    "type": "button",
+                  },
+                ],
+                "layout": "horizontal",
+                "type": "box",
+              },
+              "header": Object {
+                "contents": Array [
+                  Object {
+                    "color": "#AAAAAA",
+                    "gravity": "center",
+                    "size": "sm",
+                    "text": "① Looks 100% similar",
+                    "type": "text",
+                    "weight": "bold",
+                    "wrap": true,
+                  },
+                ],
+                "layout": "horizontal",
+                "paddingBottom": "md",
+                "spacing": "sm",
+                "type": "box",
+              },
+              "hero": undefined,
+              "type": "bubble",
+            },
+            Object {
+              "body": undefined,
+              "direction": "ltr",
+              "footer": Object {
+                "contents": Array [
+                  Object {
+                    "action": Object {
+                      "data": "{\\"input\\":\\"img-message\\",\\"sessionId\\":0,\\"state\\":\\"CHOOSING_ARTICLE\\"}",
+                      "displayText": "I choose ②",
+                      "label": "Choose this one",
+                      "type": "postback",
+                    },
+                    "color": "#ffb600",
+                    "style": "primary",
+                    "type": "button",
+                  },
+                ],
+                "layout": "horizontal",
+                "type": "box",
+              },
+              "header": Object {
+                "contents": Array [
+                  Object {
+                    "color": "#AAAAAA",
+                    "gravity": "center",
+                    "size": "sm",
+                    "text": "② Looks 100% similar",
+                    "type": "text",
+                    "weight": "bold",
+                    "wrap": true,
+                  },
+                ],
+                "layout": "horizontal",
+                "paddingBottom": "md",
+                "spacing": "sm",
+                "type": "box",
+              },
+              "hero": Object {
+                "size": "full",
+                "type": "image",
+                "url": "https://example.com/image.jpg",
+              },
+              "type": "bubble",
+            },
+          ],
+          "type": "carousel",
+        },
+        "type": "flex",
+      },
+    ]
+  `);
+});
