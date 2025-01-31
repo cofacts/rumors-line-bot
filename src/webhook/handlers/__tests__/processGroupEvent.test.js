4e3550f120076a476d644193973ec6fb6c26f3ef
MrOrz <johnsonliang7@gmail.com>
2025-01-31 14:40:47 +0800

    test: mock groupMessage as a jest function in processGroupEvent tests - Loading groupMessage in mock somehow keeps test running

--- src/webhook/handlers/__tests__/processGroupEvent.test.js
+++ src/webhook/handlers/__tests__/processGroupEvent.test.js
@@ -1,7 +1,7 @@
 jest.mock('src/lib/ga');
 jest.mock('src/lib/gql');
 jest.mock('src/webhook/lineClient');
-jest.mock('../groupMessage');
+jest.mock('../groupMessage', () => jest.fn());
 
 import ga from 'src/lib/ga';
 import gql from 'src/lib/gql';
